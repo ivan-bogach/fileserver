1f2ba2063dc2ae7c60a819762f38f81a29ee9c4e
ivan-bogach <catmrgerman@gmail.com>
2019-08-29 14:47:03 +0300

    asdfsdfasdf

---
+++ fs.go
@@ -0,0 +1,25 @@
+package fileserver
+
+import (
+	"fmt"
+)
+
+// type fileServer struct {
+// 	root http.FileSystem
+// 	// opt  FileServerOptions
+// }
+
+// var defaults = FileServerOptions{
+// 	ServeError: NonSpecific,
+// }
+
+// func FileServer(root http.FileSystem, opt FileServerOptions) http.Handler {
+// 	if opt.ServeError == nil {
+// 		opt.ServeError = defaults.ServeError
+// 	}
+// 	return &fileServer{root: root, opt: opt}
+// }
+
+func print() {
+	fmt.Println("AKLJHKLKLJKLJKLJKLJKLJKLJ")
+}
